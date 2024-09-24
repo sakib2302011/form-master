5ad69f36dcd00c51f716ddc2d79302d15a6f53ca
NazmusSakib <n.sakib.sad@gmail.com>
2024-09-25 00:24:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import SimpleForm from './Form-control/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Now it&apos;s time to take</h1>
+      <SimpleForm></SimpleForm>
+      
+    </>
+  )
+}
+
+export default App
